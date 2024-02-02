e46aecb6a5ae3d924e8a4d72e72ead337c8fed9c
Mikhail Sarnov <kpblm2@gmail.com>
2024-02-02 16:14:13 +0100

    security issues1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def exec_command():
     if not command:
         abort(400, "Invalid command")
 
-    subprocess.run(command, shell=True)
+    subprocess.run(command, shell=False)
     return "Kommando ausgeführt\n"
 
 @app.route('/upload', methods=['POST'])
@@ -36,4 +36,5 @@ def run_command():
     return "Kommando ausgeführt\n"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
+
